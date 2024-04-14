73b3868618f65ed93771601fcf39db90927db457
Dan P <158219974+Danielfww@users.noreply.github.com>
2024-04-14 08:28:09 -0400

    Create app.js
    Trying JS code for the first time

---
+++ app.js
@@ -0,0 +1,18 @@
+//Kelvin temperature 
+const kelvin = 293;
+
+//Celsius temperature calculated
+var celsius = 0 - 273;
+console.log(celsius);
+//Fahrenheit temperature calculated
+fahrenheit = celsius *(9/5) + 32;
+console.log(fahrenheit);
+//using math floor to round the number
+fahrenheit = Math.floor(fahrenheit);
+console.log(fahrenheit);
+console.log(`The tempereature is ${fahrenheit} degrees Fehrenheit`)
+console.log(kelvin);
+console.log(celsius);
+console.log(fahrenheit);
+// kelvin - celsius  = -273 in Celsius Task 11
+fahrenheit = celsius *(9/5) +32;
